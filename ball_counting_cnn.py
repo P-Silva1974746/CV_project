15552804b6a44761f0b8d43d3a3aa32bfed1481e
P-Silva1974746 <pedrofsilva5164@gmail.com>
2026-06-27 10:58:52 +0100

    ADD: baseline models and correct input output expected when using inference only

--- ball_counting_cnn.py
+++ ball_counting_cnn.py
@@ -25,6 +25,7 @@ from torchvision import transforms
 import torch
 
 from visualization import plot_history
+from cnn_basline import SimpleCNN, BaselineCNN
 
 
 def get_image_metadata(
@@ -76,10 +77,15 @@ def get_image_metadata(
 
                         
 class ApplyTransform(torch.utils.data.Dataset):
-    def __init__(self, subset, transform):
+    def __init__(self, subset, transform, inference_only=False):
         self.subset = subset
         self.transform = transform
+        self._inference_only = inference_only
     def __getitem__(self, index):
+        if self._inference_only:
+            x = self.subset[index]
+            return self.transform(x)
+        
         x, y = self.subset[index]
         return self.transform(x), y
     def __len__(self):
@@ -88,18 +94,18 @@ class ApplyTransform(torch.utils.data.Dataset):
 
 class Pool_table_Dataset(Dataset):
     """Simple dataset wrapper expecting a metadata list with all the information necessary
-
-    Each of those directories should contain image files. This class applies basic
-    transformations such as resizing and normalization.
     """
 
-    def __init__(self, metadata:list[dict], transform: Optional[transforms.Compose] = None):
+    def __init__(self, metadata:list[dict], transform: Optional[transforms.Compose] = None, inference_only = False):
         self._meta_data = metadata
-
+        self._inference_only = inference_only
         self.samples = []  # list of (path, count)
         
         for d in  self._meta_data:
-            self.samples.append((os.path.join('./', d['full_path']), d['total_balls']))
+            if not inference_only:
+                self.samples.append((os.path.join('./', d['full_path']), d['total_balls']))
+            else:
+                self.samples.append(os.path.join('./', d))
 
         self.train_transform = transform or v2.Compose([
             v2.ToImage(),
@@ -130,9 +136,17 @@ class Pool_table_Dataset(Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        path, count = self.samples[idx]
+        if not self._inference_only:
+            path, count = self.samples[idx]
+        else:
+            path = self.samples[idx]
+
         image = Image.open(path).convert("RGB")
-        return image, count
+
+        if not self._inference_only:
+            return image, count
+        else:
+            return image
         
 
 
@@ -141,7 +155,7 @@ def build_regression_model(backbone='resnet18', pretrained=True):
         model = models.resnet18(weights='DEFAULT' if pretrained else None)
         model.fc = nn.Sequential(
             nn.Dropout(0.3),
-            nn.Linear(model.fc.in_features, 1)   # single count output
+            nn.Linear(model.fc.in_features, 1)
         )
     elif backbone == 'efficientnet_b0':
         model = models.efficientnet_b0(weights='DEFAULT' if pretrained else None)
@@ -150,8 +164,23 @@ def build_regression_model(backbone='resnet18', pretrained=True):
             nn.Dropout(0.3),
             nn.Linear(in_features, 1)
         )
+    elif backbone == 'baseline_1':
+        model = SimpleCNN()
+    elif backbone == 'baseline_2':
+        model = BaselineCNN()
     return model
 
+def make_preds(model, dataloader, device):
+    model.eval()
+    all_preds = []
+    with torch.no_grad():
+        for images in dataloader:
+            images = images.to(device)
+            outputs = model(images)
+            preds = outputs.squeeze(1).round().cpu().numpy()
+            all_preds.extend(preds.tolist())
+
+    return all_preds
 
 def evaluate_model(model, dataloader, device):
     model.eval()
@@ -177,19 +206,20 @@ def evaluate_model(model, dataloader, device):
 
     excluded = len(all_labels) - len(non_zero)
     print(f"MAPE computed on {len(non_zero)} samples ({excluded} zero-label samples excluded)")
-    return mae, mape, mse
+    return mae, mape, mse, all_preds
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Ball counting training pipeline")
     parser.add_argument("--data-dir", type=str, required=True, help="Root directory of dataset")
     parser.add_argument("--output-dir", type=str, required=True, help="Directory where models are saved")
-    parser.add_argument("--backbone", type=str, required=True, help="Architecture of the cnn model")
+    parser.add_argument("--backbone", type=str, default="efficientnet_b0", help="Architecture of the cnn model")
     parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--batch-size", type=int, default=32)
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--pretrained", action=argparse.BooleanOptionalAction, default=True)
     parser.add_argument("--train", action=argparse.BooleanOptionalAction, default=True)
+    parser.add_argument("--inference-only", action=argparse.BooleanOptionalAction, default=True)
     parser.add_argument("--no-cuda", action="store_true")
     return parser.parse_args()
 
@@ -202,83 +232,99 @@ if __name__ == "__main__":
     os.makedirs(args.output_dir, exist_ok=True)
     DATA_PATH = args.data_dir
 
-    #------------------- METADATA EXTRACTING BLOCK -------------------
-    datasets = os.listdir(DATA_PATH)
-    total_metadata = []
-    for dataset in datasets:
-        print("="*90)
-        print(f"For dataset: {dataset}")
-        print("="*90)
-        splits = os.listdir(os.path.join(DATA_PATH, dataset))
-        print(splits)
-        for split in splits:
-            if not split.endswith(".txt"):
-                img_pths = os.listdir(os.path.join(DATA_PATH, dataset, split))
-                annotation_path = os.path.join(DATA_PATH, dataset, split, "_annotations.coco.json")
-                for img_pth in img_pths:
-                    if not img_pth.endswith(".json"):
-                        try:
-                            full_path = os.path.join(DATA_PATH, dataset, split, img_pth)
-                            meta = get_image_metadata(
-                                target_filename=img_pth,
-                                dataset=dataset,
-                                annotation_path=annotation_path,
-                                split_name=split,
-                                full_image_path=full_path,
-                            )
-                            if isinstance(meta, dict):
-                                total_metadata.append(meta)
-                        except Exception as e:
-                            print(("="*10+"ERROR")*5+"="*10)
-                            print(traceback.format_exc())
-                            print(("="*10+"ERROR")*5+"="*10)
-                            break
-    #------------------- METADATA EXTRACTING BLOCK -------------------
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    if not args.inference_only:
+        #------------------- METADATA EXTRACTING BLOCK -------------------
+        datasets = os.listdir(DATA_PATH)
+        total_metadata = []
+        for dataset in datasets:
+            print("="*90)
+            print(f"For dataset: {dataset}")
+            print("="*90)
+            splits = os.listdir(os.path.join(DATA_PATH, dataset))
+            print(splits)
+            for split in splits:
+                if not split.endswith(".txt"):
+                    img_pths = os.listdir(os.path.join(DATA_PATH, dataset, split))
+                    annotation_path = os.path.join(DATA_PATH, dataset, split, "_annotations.coco.json")
+                    for img_pth in img_pths:
+                        if not img_pth.endswith(".json"):
+                            try:
+                                full_path = os.path.join(DATA_PATH, dataset, split, img_pth)
+                                meta = get_image_metadata(
+                                    target_filename=img_pth,
+                                    dataset=dataset,
+                                    annotation_path=annotation_path,
+                                    split_name=split,
+                                    full_image_path=full_path,
+                                )
+                                if isinstance(meta, dict):
+                                    total_metadata.append(meta)
+                            except Exception as e:
+                                print(("="*10+"ERROR")*5+"="*10)
+                                print(traceback.format_exc())
+                                print(("="*10+"ERROR")*5+"="*10)
+                                break
+        #------------------- METADATA EXTRACTING BLOCK -------------------
+        
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    # fixed seed for the split of the dataset
-    generator = torch.Generator().manual_seed(42)
+        # fixed seed for the split of the dataset
+        generator = torch.Generator().manual_seed(42)
 
-    print(f"Running on device: {device}")
+        print(f"Running on device: {device}")
 
-    # dataset and splits, mantaining main_.coco test is in the final test plit
-    main_test_meta = [m for m in total_metadata if m['split'] == 'test']
-    trainval_meta  = [m for m in total_metadata if not (m['dataset'] == 'main_.coco' and m['split'] == 'test')]
+        # dataset and splits, mantaining main_.coco test is in the final test plit
+        main_test_meta = [m for m in total_metadata if m['split'] == 'test']
+        trainval_meta  = [m for m in total_metadata if not (m['dataset'] == 'main_.coco' and m['split'] == 'test')]
 
 
-    print(f"Test set: {len(main_test_meta)} samples")
-    print(f"Train/val:            {len(trainval_meta)} samples")
+        print(f"Test set: {len(main_test_meta)} samples")
+        print(f"Train/val:            {len(trainval_meta)} samples")
 
-    trainval_dataset = Pool_table_Dataset(trainval_meta)
-    test_dataset     = Pool_table_Dataset(main_test_meta)
+        trainval_dataset = Pool_table_Dataset(trainval_meta)
+        test_dataset     = Pool_table_Dataset(main_test_meta)
 
-    total_train_val = len(trainval_dataset)
-    n_train = int(total_train_val * 0.9)
-    n_val   = total_train_val - n_train
+        total_train_val = len(trainval_dataset)
+        n_train = int(total_train_val * 0.9)
+        n_val   = total_train_val - n_train
 
-    train_set, val_set = random_split(
-        trainval_dataset, [n_train, n_val], generator=generator
-    )
+        train_set, val_set = random_split(
+            trainval_dataset, [n_train, n_val], generator=generator
+        )
 
-    train_loader = DataLoader(
-        ApplyTransform(train_set, trainval_dataset.train_transform),
-        batch_size=args.batch_size, shuffle=True, num_workers=4
-    )
-    val_loader = DataLoader(
-        ApplyTransform(val_set, trainval_dataset.val_transform),
-        batch_size=args.batch_size, shuffle=False, num_workers=4
-    )
-    test_loader = DataLoader(
-        ApplyTransform(test_dataset, test_dataset.val_transform),
-        batch_size=args.batch_size, shuffle=False, num_workers=4
-    )
+        train_loader = DataLoader(
+            ApplyTransform(train_set, trainval_dataset.train_transform),
+            batch_size=args.batch_size, shuffle=True, num_workers=4
+        )
+        val_loader = DataLoader(
+            ApplyTransform(val_set, trainval_dataset.val_transform),
+            batch_size=args.batch_size, shuffle=False, num_workers=4
+        )
+        test_loader = DataLoader(
+            ApplyTransform(test_dataset, test_dataset.val_transform),
+            batch_size=args.batch_size, shuffle=False, num_workers=4
+        )
 
-    # clear memory before training
-    del total_metadata, trainval_dataset, test_dataset, main_test_meta, trainval_meta
-    gc.collect()
-    
-    if args.train:
+        # clear memory before training
+        del total_metadata, trainval_dataset, test_dataset, main_test_meta, trainval_meta
+        gc.collect()
+    else:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Running on device: {device}")
+        print(f"Running inference only!!!")
+
+        with open(DATA_PATH, 'r') as f:
+            paths = json.load(f)
+        ims_path = paths['image_path']
+
+        test_dataset = Pool_table_Dataset(ims_path, inference_only=True)
+
+        test_loader = DataLoader(
+            ApplyTransform(test_dataset, test_dataset.val_transform, inference_only=True),
+            batch_size=args.batch_size, shuffle=False, num_workers=4
+        )
+
+    if args.train and not args.inference_only:
 
         model = build_regression_model(backbone=args.backbone, pretrained=args.pretrained)
 
@@ -287,9 +333,8 @@ if __name__ == "__main__":
         criterion = nn.MSELoss()
         optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-        if not args.pretrained:
-            # reduces LR by a factor of 10 if val_loss doesn't improve for 5 epochs
-            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5)
+        # reduces LR by a factor of 10 if val_loss doesn't improve for 5 epochs
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10)
 
         history = {
             "train_loss": [],
@@ -347,13 +392,13 @@ if __name__ == "__main__":
             val_loss = val_loss / val_total
             val_acc = val_correct / val_total
 
-            if not args.pretrained:
-                scheduler.step(val_loss)
+            scheduler.step(val_loss)
 
             # ------ Early Stopping Logic ------
             if val_loss < best_val_loss_for_es:
                 best_val_loss_for_es = val_loss
                 epochs_no_improve = 0
+                torch.save(model.state_dict(), os.path.join(args.output_dir, "best_model.pth"))
             else:
                 epochs_no_improve += 1
                 print(f"Early stopping patience: {epochs_no_improve}/{early_stopping_patience}")
@@ -375,12 +420,6 @@ if __name__ == "__main__":
                   f"val_loss: {val_loss:.4f}, val_acc: {val_acc:.4f} - "
                   f"time {elapsed:.2f}s")
 
-            if val_acc > best_val_acc:
-                best_val_acc = val_acc
-                torch.save(model.state_dict(), os.path.join(args.output_dir, "best_model.pth"))
-
-            # if device.type == 'cpu': # this is usefull for limited memory scenarios
-            #     gc.collect()
         #--------------- TRAINING BLOCK ---------------
 
         plot_history(history=history, output_dir=args.output_dir)
@@ -391,13 +430,35 @@ if __name__ == "__main__":
     # test set evaluation
     model = build_regression_model(backbone=args.backbone, pretrained=False)
     model.load_state_dict(torch.load(os.path.join(args.output_dir, "best_model.pth"), map_location=device))
-    mae, mape, mse = evaluate_model(model, test_loader, device)
-    print(f"Test mae: {mae:.4f}")
-    print(f"Test mape: {mape:.4f}")
-    print(f"Test mse: {mse:.4f}")
+    if not args.inference_only:
+        mae, mape, mse, all_preds = evaluate_model(model, test_loader, device)
+        print(f"Test mae: {mae:.4f}")
+        print(f"Test mape: {mape:.4f}")
+        print(f"Test mse: {mse:.4f}")
+    else:
+        all_preds = make_preds(model, test_loader, device)
 
-    with open(os.path.join(args.output_dir,"metrics.txt"), mode='w') as f:
-        f.write(f"""Test mae: {mae:.4f}
+    output_json_data = []
+    for i, path in enumerate(ims_path):
+        output_json_data.append({
+            "image_path": path,
+            "num_balls": all_preds[i],
+        })
+
+    # --- NEW: Save the JSON file ---
+    with open(os.path.join(args.output_dir, "predictions.json"), "w") as outfile:
+        json.dump(output_json_data, outfile, indent=4)
+    print("="*30)
+    print(f"Saved predictions to {os.path.join(args.output_dir, "predictions.json")}")
+    print("="*30)
+    # --- NEW: Save the JSON file ---
+
+    if not args.inference_only:
+        with open(os.path.join(args.output_dir,"metrics.txt"), mode='w') as f:
+            f.write(f"""Test mae: {mae:.4f}
 Test mape: {mape:.4f}
 Test mse: {mse:.4f}""")
+        print("="*30)
+        print(f"Saved Metrics to {os.path.join(args.output_dir,"metrics.txt")}")
+        print("="*30)
         
